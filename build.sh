1cb484950dbb89072991af64b607fbddb47916f8
tiffanypark <30330876+tiffanypark@users.noreply.github.com>
2017-08-11 18:45:26 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 set -e -x
-
-pushd sysmaster-web-git
-  ./mvnw clean package
-popd
-
+./sysmaster-web-git/mvnw clean package
 cp sysmaster-web-gite/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar  build-output/.
